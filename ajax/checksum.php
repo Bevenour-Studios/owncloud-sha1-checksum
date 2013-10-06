63fc67161a96b0e6d635f0f5256f974f5d5eea92
E.J. Bevenour <ejbevenour@live.com>
2013-10-06 19:40:41 -0400

    Update checksum.php

--- ajax/checksum.php
+++ ajax/checksum.php
@@ -9,7 +9,7 @@ $source = $_GET['source'];
 $dir = $_GET['dir'];
 $file = $dir.$source;
 if($info = \OC\Files\Filesystem::getLocalFile($file)){
-  $md5 = md5_file($info);
+  $md5 = sha1_file($info);
   OCP\JSON::success(array('data' => array($md5)));
 } else {
   OCP\JSON::error(array('data' => array('An Error occured.')));
